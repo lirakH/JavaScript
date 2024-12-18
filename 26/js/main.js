a9169a3a3080d6d6380e2cdb4a13ad1284b3e41b
lirakH <135222007+lirakH@users.noreply.github.com>
2024-12-18 16:43:45 +0100

    Add files via upload

---
+++ 26/js/main.js
@@ -0,0 +1,59 @@
+
+console.log("Hello World!!");
+
+alert("Page is open!");
+
+
+// JavaScript variables
+
+/*
+
+A variable is a “named storage” for data.
+Placeholder
+
+six data types are Primitive(predefined).
+
+	1. Numbers: 5, 6.5, 7, etc.
+
+	2. String: “Hello GeeksforGeeks” etc.
+
+	3. Boolean: Represent a logical entity and can have two values: true or false.
+
+	4. Null: This type has only one value: null.
+
+	5. Undefined: A variable that has not been assigned a value is undefined.
+
+	6. Object: It is the most important data type and forms the building blocks for modern JavaScript.
+
+
+
+There are some rules while declaring a JavaScript variable (also known as identifiers).
+
+	➣The name must start with a letter (a to z or A to Z), underscore( _ ), or dollar( $ ) sign.
+
+	➣After the first letter we can use digits (0 to 9), for example, value1.
+
+	➣JavaScript variables are case sensitive, for example, x and X are different variables.
+
+*/
+
+var numri = 4; //Number
+
+var _emri = "Liraku"; //String
+
+var $visible = false; //Boolean
+
+var numri2 = null; //Null
+
+var A;  //Undefined
+
+console.log(numri);
+console.log("numri");
+console.log(_emri);
+console.log(numri2);
+console.log(A);
+//console.log(a);
+
+
+
+
